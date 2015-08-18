86ffd8bca16d4bd942e080d6883e38bc1df9d7d0
Mateus Barbosa <mateusbarbosa96@gmail.com>
2015-08-18 16:39:55 -0300

    .

--- PCS3111/Mensagem.cpp
+++ PCS3111/Mensagem.cpp
@@ -8,3 +8,29 @@ Turma 23
  	Daniel Nery Silva de Oliveira - 9349051
 	Mateus Almeida Barbosa        - 9349072
 **/
+
+#include <iostream>
+#include "Mensagem.hpp"
+
+using namespace std;
+
+namespace Polikut {
+    Mensagem::Mensagem() {}
+
+    Mensagem::Mensagem(string conteudo) {
+        this->conteudo = conteudo;
+        this->curtidas = 0;
+    }
+
+    string Mensagem::getConteudo() {
+        return this->conteudo;
+    }
+
+    int Mensagem::getCurtidas() {
+        return curtidas;
+    }
+
+    void Mensagem::curtir() {
+        curtidas++;
+    }
+}
