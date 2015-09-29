2e2655675fd9f318df8b6b22774f0a45995a1a64
Daniel Nery <danielnso@outlook.com>
2015-09-28 21:34:19 -0300

    Arquivos de classes novas c/ cabecalho

---
+++ PCS3111/MensagemComCurtir.cpp
@@ -0,0 +1,13 @@
+/**
+Escola Politecnica da Universidade de Sao Paulo
+14 de outubro de 2015
+
+PCS3111 - Laboratorio de Programacao Orientada a Objetos
+Exercicio Programa 2 - 2015
+
+Turma 23
+	Daniel Nery Silva de Oliveira - 9349051
+	Mateus Almeida Barbosa        - 9349072
+
+Professor Jaime S. Sichman
+**/
