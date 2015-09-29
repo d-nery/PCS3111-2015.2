9463abe72c747385ad06cd76b8953e618bdc2db0
Mateus Barbosa <mateusbarbosa96@gmail.com>
2015-09-29 15:53:26 -0300

    Implementação do Perfil

--- PCS3111/Perfil.cpp
+++ PCS3111/Perfil.cpp
@@ -11,3 +11,53 @@ Turma 23
 
 Professor Jaime S. Sichman
 **/
+
+#include <iostream>
+
+#include "Perfil.hpp"
+
+namespace Polikut {
+	Perfil::Perfil(std::string nome) {
+		this->nome = nome;
+	}
+
+	Perfil::~Perfil() {}
+
+	std::string Perfil::getNome() {
+		return this->nome;
+	}
+
+	void Perfil::envia(std::string texto, bool curtir) {
+		Mensagem* mensagem;
+		if (curtir) {
+			mensagem = new MensagemComCurtir()
+		}
+        for (int i = 0; i < this->numeroContatos; i++)
+            contatos[i]->recebe(mensagem);
+        this->enviadas.adicionar(mensagem);
+	}
+
+	void Perfil::recebe(Mensagem* m) {
+		this->recebidas.adicionar(m);
+	}
+
+	ListaDeMensagens& Perfil::getMensagensRecebidas() {
+
+
+	}
+
+	ListaDeMensagens& Perfil::getMensagensEnviadas() {
+
+
+	}
+
+	void Perfil::verContatos() {
+
+
+	}
+
+	void Perfil::verContatosAlcancaveis() {
+
+
+	}
+}
